dbd834385b36b6170cc7148a7e625bf3ec3562f5
vijay229 <kumarvijay34453@gmail.com>
2025-03-31 22:42:44 +0530

    Make quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -83,10 +83,13 @@ document.querySelectorAll('.js_add_to_cart').forEach((button) => {
       });
 
     }
-    
-    
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity +=item.quantity;
 
 
-    console.log(cart);
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
